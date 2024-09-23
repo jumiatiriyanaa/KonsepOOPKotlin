1cf96d5ef16d7cb370540df2c29c89f7a6340430
Jumiati Riyana <jumiatiriyanaa@gmail.com>
2024-09-23 17:11:53 +0700

    Add Greeter.kt

--- src/main/kotlin/id/ac/polbeng/jumiati/test_kelas/Greeter.kt
+++ src/main/kotlin/id/ac/polbeng/jumiati/test_kelas/Greeter.kt
@@ -1,7 +1,15 @@
 package id.ac.polbeng.jumiati.test_kelas
 
 class Greeter {
-    fun greet(){
+
+    var text: String = ""
+    fun greet() {
         println("Hello object world!")
+    }
+    fun greet(name: String){
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
     }
 }
