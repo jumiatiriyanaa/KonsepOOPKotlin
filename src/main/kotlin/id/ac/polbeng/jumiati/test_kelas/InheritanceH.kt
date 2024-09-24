f10606f6c6ed56c21d0965632b1e21d012115172
Jumiati Riyana <jumiatiriyanaa@gmail.com>
2024-09-25 00:00:42 +0700

    Add InheritanceH.kt

---
+++ src/main/kotlin/id/ac/polbeng/jumiati/test_kelas/InheritanceH.kt
@@ -0,0 +1,25 @@
+package id.ac.polbeng.jumiati.test_kelas
+
+open class Rectangle {
+    open fun draw(){
+        println("From Base Class Rectangle")
+    }
+}
+
+interface  Polygon {
+    fun draw() {
+        println("From interface Polygon")
+    }
+}
+
+class Square() : Rectangle(), Polygon{
+    override fun draw() {
+        super<Rectangle>.draw()
+        super<Polygon>.draw()
+    }
+}
+
+fun main() {
+    val square = Square()
+    square.draw()
+}
